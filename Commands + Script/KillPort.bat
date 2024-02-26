64739df8aaf0cbc9f1228edee0b9a9393ccdebbd
ILAN <kumaranilan@yahoo.com>
2024-02-26 08:58:49 +0000

    Update KillPort.bat

--- Commands + Script/KillPort.bat
+++ Commands + Script/KillPort.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 TITLE Kill Port
 
-set /p port="ENTER PORT TO BE KILLED ::::'
-ECHO %port%
+set /p port="ENTER THE PORT NUMBER THAT NEED TO BE KILLED :::: "
+ECHO you have entered the port number ::: %port%
 
 FOR /F "tokens=5 delims= " %%P IN ('netstat -ano ^| findstr :%port%') DO taskkill /PID %%P /F
 
